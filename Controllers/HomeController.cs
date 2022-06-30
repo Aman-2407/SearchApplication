a40f90affbc23f426d57df511938e25633647493
Aman <av986764@gmail.com>
2022-06-30 21:28:41 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SearchApplication.Models;
+
+namespace SearchApplication.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public JsonResult Getemp(string Recipe_name)
+		{
+			Search_dbEntities sdb = new Search_dbEntities();
+			var recipe = (from x in sdb.Recipes where x.Recipe_name.StartsWith(Recipe_name) select new { label = x.Recipe_name }).ToList();
+			return Json(recipe);
+		}
+
+		
+	}
+}
---
+++ Models/RecipeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SearchApplication.Models
+{
+	public class RecipeModel
+	{
+		public int Recipe_id { get; set; }
+		public string Recipe_name { get; set; }
+	}
+}
